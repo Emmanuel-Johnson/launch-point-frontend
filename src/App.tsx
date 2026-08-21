7615ec9a76a864e5364a9daa53d8a2e7fc093f30
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2026-08-21 12:11:40 +0530

    chore: configure React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <h1 className="text-4xl font-bold text-violet-600">
-      Launch Point
-    </h1>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
