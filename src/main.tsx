ff887a8df7a929ed930362b31d74c58741b35d53
Emmanuel Johnson <emmanuel.johnson.pro@gmail.com>
2026-09-14 18:00:20 +0530

    feat: add Google authentication with account linking

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "./index.css";
 import App from "./App";
@@ -8,7 +9,9 @@ import App from "./App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
